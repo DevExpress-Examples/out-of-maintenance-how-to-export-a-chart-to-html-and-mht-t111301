8975c469068c3ec3cd1d7d6af58d34f487cfb033
DevExpressExampleBot <fakeEmail>
2022-12-18 18:53:00 +0400

    Source auto update [skip ci]

--- VB/Export/MainWindow.xaml.vb
+++ VB/Export/MainWindow.xaml.vb
@@ -1,4 +1,4 @@
-﻿Imports System.Diagnostics
+Imports System.Diagnostics
 Imports System.IO
 Imports System.Windows
 Imports DevExpress.Xpf.Charts
@@ -6,51 +6,44 @@ Imports DevExpress.XtraPrinting
 
 Namespace Export
 
-	Partial Public Class MainWindow
-		Inherits Window
+    Public Partial Class MainWindow
+        Inherits Window
 
-		Public Sub New()
-			InitializeComponent()
-		End Sub
+        Public Sub New()
+            Me.InitializeComponent()
+        End Sub
 
-		Private Sub ExportChartToHTML(ByVal chart As ChartControl)
+        Private Sub ExportChartToHTML(ByVal chart As ChartControl)
+            ' Create an object containing HTML export options.
+            Dim htmlOptions As HtmlExportOptions = New HtmlExportOptions()
+            ' Set HTML-specific export options.
+            htmlOptions.CharacterSet = "utf-8"
+            htmlOptions.RemoveSecondarySymbols = False
+            htmlOptions.Title = "Unicode UTF-8 Example"
+            ' Specify print size mode.
+            Dim sizeMode As PrintSizeMode = PrintSizeMode.Stretch
+            ' Export a chart to an HTML file.
+            chart.ExportToHtml("OutputUnicode.html", htmlOptions, sizeMode)
+            Call Process.Start("OutputUnicode.html")
+        End Sub
 
-			' Create an object containing HTML export options.
-			Dim htmlOptions As New HtmlExportOptions()
+        Private Sub ExportChartToMHT(ByVal chart As ChartControl)
+            ' Create an object containing MHT export options.
+            Dim mhtOptions As MhtExportOptions = New MhtExportOptions()
+            ' Specify print size mode.
+            Dim sizeMode As PrintSizeMode = PrintSizeMode.ProportionalZoom
+            ' Export a chart to a stream as MHT.
+            Dim mhtStream As FileStream = New FileStream("OutputDefault.mht", FileMode.Create)
+            chart.ExportToMht(mhtStream, mhtOptions, sizeMode)
+            Call Process.Start("OutputDefault.mht")
+        End Sub
 
-			' Set HTML-specific export options.
-			htmlOptions.CharacterSet = "utf-8"
-			htmlOptions.RemoveSecondarySymbols = False
-			htmlOptions.Title = "Unicode UTF-8 Example"
+        Private Sub Button_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
+            Me.ExportChartToHTML(Me.chartControl)
+        End Sub
 
-			' Specify print size mode.
-			Dim sizeMode As PrintSizeMode = PrintSizeMode.Stretch
-
-			' Export a chart to an HTML file.
-			chart.ExportToHtml("OutputUnicode.html", htmlOptions, sizeMode)
-			Process.Start("OutputUnicode.html")
-		End Sub
-
-		Private Sub ExportChartToMHT(ByVal chart As ChartControl)
-
-			' Create an object containing MHT export options.
-			Dim mhtOptions As New MhtExportOptions()
-
-			' Specify print size mode.
-			Dim sizeMode As PrintSizeMode = PrintSizeMode.ProportionalZoom
-
-			' Export a chart to a stream as MHT.
-			Dim mhtStream As New FileStream("OutputDefault.mht", FileMode.Create)
-			chart.ExportToMht(mhtStream, mhtOptions, sizeMode)
-			Process.Start("OutputDefault.mht")
-		End Sub
-
-		Private Sub Button_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
-			ExportChartToHTML(chartControl)
-		End Sub
-
-		Private Sub Button_Click_1(ByVal sender As Object, ByVal e As RoutedEventArgs)
-			ExportChartToMHT(chartControl)
-		End Sub
-	End Class
+        Private Sub Button_Click_1(ByVal sender As Object, ByVal e As RoutedEventArgs)
+            Me.ExportChartToMHT(Me.chartControl)
+        End Sub
+    End Class
 End Namespace
